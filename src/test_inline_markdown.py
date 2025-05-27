260711d062c340c39d326ca5906c0586d2998bdb
DnBaal <dantheassassin2000@gmail.com>
2025-05-27 13:06:21 +0200

    feat: add LeafNode, ParentNode, and conversion from TextNode and raw markdown

--- src/test_inline_markdown.py
+++ src/test_inline_markdown.py
@@ -8,7 +8,7 @@ class TestInlineMarkdown(unittest.TestCase):
     def test_delim_code(self):
         node = TextNode("This is a text with a `code block` word", TextType.TEXT)
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        self.assertEqual(
+        self.assertListEqual(
             [
                 TextNode("This is a text with a ", TextType.TEXT),
                 TextNode("code block", TextType.CODE),
@@ -22,7 +22,7 @@ class TestInlineMarkdown(unittest.TestCase):
             "This is a text with `code block` and `another code` word", TextType.TEXT
         )
         new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        self.assertEqual(
+        self.assertListEqual(
             [
                 TextNode("This is a text with ", TextType.TEXT),
                 TextNode("code block", TextType.CODE),
