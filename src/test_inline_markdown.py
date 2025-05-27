e1a31d346dc4af3c3397a5e94320626e62df69d5
DnBaal <dantheassassin2000@gmail.com>
2025-05-27 12:51:21 +0200

    feat: add LeafNode, ParentNode, and conversion from TextNode and raw markdown

---
+++ src/test_inline_markdown.py
@@ -0,0 +1,41 @@
+import unittest
+
+from inline_markdown import split_nodes_delimiter
+from textnode import TextNode, TextType
+
+
+class TestInlineMarkdown(unittest.TestCase):
+    def test_delim(self):
+        node = TextNode("This is a text with a `code block` word", TextType.TEXT)
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+            [
+                TextNode("This is a text with a ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_multiple_delimiters(self):
+        node = TextNode(
+            "This is a text with `code block` and `another code` word", TextType.TEXT
+        )
+        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
+        self.assertEqual(
+            [
+                TextNode("This is a text with ", TextType.TEXT),
+                TextNode("code block", TextType.CODE),
+                TextNode(" and ", TextType.TEXT),
+                TextNode("another code", TextType.CODE),
+                TextNode(" word", TextType.TEXT),
+            ],
+            new_nodes,
+        )
+
+    def test_delim_invalid_syntax(self):
+        node = TextNode(
+            "This is a text with `code block` and `another code", TextType.TEXT
+        )
+        with self.assertRaises(ValueError):
+            split_nodes_delimiter([node], "`", TextType.CODE)
--- src/test_textnode.py
+++ src/test_textnode.py
@@ -1,6 +1,5 @@
 import unittest
 
-from split_node_delimiter import split_nodes_delimiter
 from textnode import TextNode, TextType, text_node_to_html_node
 
 
@@ -60,41 +59,6 @@ class TestTextNode(unittest.TestCase):
             html_node.props,
             {"src": "https://example.com/image.png", "alt": "Hello image"},
         )
-
-    def test_split_node_delimiter(self):
-        node = TextNode("This is a text with a `code block` word", TextType.TEXT)
-        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        self.assertEqual(
-            [
-                TextNode("This is a text with a ", TextType.TEXT),
-                TextNode("code block", TextType.CODE),
-                TextNode(" word", TextType.TEXT),
-            ],
-            new_nodes,
-        )
-
-    def test_split_node_delimiter_multiple_delimiters(self):
-        node = TextNode(
-            "This is a text with `code block` and `another code` word", TextType.TEXT
-        )
-        new_nodes = split_nodes_delimiter([node], "`", TextType.CODE)
-        self.assertEqual(
-            [
-                TextNode("This is a text with ", TextType.TEXT),
-                TextNode("code block", TextType.CODE),
-                TextNode(" and ", TextType.TEXT),
-                TextNode("another code", TextType.CODE),
-                TextNode(" word", TextType.TEXT),
-            ],
-            new_nodes,
-        )
-
-    def test_split_node_delimiter_invalid_syntax(self):
-        node = TextNode(
-            "This is a text with `code block` and `another code", TextType.TEXT
-        )
-        with self.assertRaises(ValueError):
-            split_nodes_delimiter([node], "`", TextType.CODE)
 
 
 if __name__ == "__main__":
